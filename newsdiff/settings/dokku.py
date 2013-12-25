c6253c8fba57f16bd99e524d8cd31d808bc834ac
Yuval Adam <yuv.adm@gmail.com>
2013-12-25 18:52:12 +0200

    Use dj_database_url on dokku

--- newsdiff/settings/dokku.py
+++ newsdiff/settings/dokku.py
@@ -1,5 +1,12 @@
+import dj_database_url
+
+from os import environ
 from .base import *
 
 ENV = 'dokku'
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
