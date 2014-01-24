2b2b75431e7f67c01e1ce70b3b7833857553481a
Yuval Adam <yuv.adm@gmail.com>
2014-01-24 16:27:14 +0100

    Add image inline admin

--- newsdiff/core/admin.py
+++ newsdiff/core/admin.py
@@ -5,10 +5,15 @@ from django.contrib import admin
 from .models import *
 
 
+class HaaretzImageline(admin.TabularInline):
+    model = HaaretzImage
+    fields = ('caption', 'image')
+
+
 class HaaretzArticleAdmin(reversion.VersionAdmin):
-    pass
+    inlines = (HaaretzImageline,)
+
 
 admin.site.register(HaaretzArticle, HaaretzArticleAdmin)
-admin.site.register(NewsDiffUser)
 admin.site.register(HaaretzImage)
-
+admin.site.register(NewsDiffUser)
