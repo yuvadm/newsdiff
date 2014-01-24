01e486b4a1309e5093ef1d14014ea0a0d001a21b
Yuval Adam <yuv.adm@gmail.com>
2014-01-25 00:56:34 +0100

    Add Ynet model admins

--- newsdiff/core/admin.py
+++ newsdiff/core/admin.py
@@ -14,6 +14,17 @@ class HaaretzArticleAdmin(reversion.VersionAdmin):
     inlines = (HaaretzImageline,)
 
 
+class YnetImageline(admin.TabularInline):
+    model = YnetImage
+    fields = ('caption', 'image')
+
+
+class YnetArticleAdmin(reversion.VersionAdmin):
+    inlines = (YnetImageline,)
+
+
 admin.site.register(HaaretzArticle, HaaretzArticleAdmin)
 admin.site.register(HaaretzImage)
+admin.site.register(YnetArticle, YnetArticleAdmin)
+admin.site.register(YnetImage)
 admin.site.register(NewsDiffUser)
