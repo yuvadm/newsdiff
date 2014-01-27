8e4452efa6733e2a5c53a233d27f034fd6294c18
Yuval Adam <yuv.adm@gmail.com>
2014-01-27 12:50:06 +0200

    Go away bots

--- newsdiff/urls.py
+++ newsdiff/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 
 from newsdiff.core.views import *
 
@@ -10,6 +11,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^haaretz/(?P<id>[\d\.]+)$', HaaretzArticleView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots.txt', lambda x: HttpResponse('User-agent: *\nDisallow: /', mimetype='text/plain'))
 )
 
 if settings.ENV == 'local':
