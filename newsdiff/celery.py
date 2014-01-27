737cb4dbcb1ab8f18588ec5a4e5920a418fc0a7e
Yuval Adam <yuv.adm@gmail.com>
2014-01-27 23:47:39 +0200

    Add raven to celery tasks

--- newsdiff/celery.py
+++ newsdiff/celery.py
@@ -4,9 +4,14 @@ import os
 
 from celery import Celery
 from django.conf import settings
+from raven import Client
+from raven.contrib.celery import register_signal
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsdiff.settings')
 
 app = Celery('newsdiff')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+raven_client = Client(os.environ.get('SENTRY_URL'))
+register_signal(raven_client)
