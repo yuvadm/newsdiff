b35021b110605716b452a99f53d3758318a70d5b
Yuval Adam <yuv.adm@gmail.com>
2014-01-27 12:26:10 +0200

    Add haaretz diff view

--- newsdiff/core/views.py
+++ newsdiff/core/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import reversion
 
-# Create your views here.
+from django.views.generic import DetailView
+from reversion.helpers import generate_patch_html
+from reversion.models import Version
+from .models import *
+
+
+class HaaretzArticleView(DetailView):
+
+    context_object_name = 'article'
+    model = HaaretzArticle
+    slug_field = 'haaretz_id'
+    slug_url_kwarg = 'id'
+
+    template_name = 'haaretz-article.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HaaretzArticleView, self).get_context_data(**kwargs)
+        versions = reversion.get_for_object(self.object)
+        version_diffs = [(v1, v2) for v1, v2 in zip(versions, versions[1:])]
+        print dir(version_diffs[0])
+        context['diffs'] = [{
+            'diff': generate_patch_html(v1, v2, 'text', cleanup='efficiency')
+        } for (v1, v2) in version_diffs]
+        # context['diffs'] = [(v1, v2) for v1, v2 in zip(versions, versions[1:])]
+        return context
--- newsdiff/urls.py
+++ newsdiff/urls.py
@@ -3,9 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from newsdiff.core.views import *
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^haaretz/(?P<id>[\d\.]+)$', HaaretzArticleView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
 
